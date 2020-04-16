2ec26bc552fe73537cb1902d54a4a58cfd503f34
Taishawn King <taishawnking1@yahoo.com>
2020-04-16 16:55:59 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -11,7 +11,7 @@ def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-return "Select name, age From bears Where age = 20 And name != 'nil'" 
+return "Select name, age From bears Where age = 20 And name != 'nil'"; 
 end
 
 def select_youngest_bear_and_returns_name_and_age
@@ -27,6 +27,6 @@ def counts_number_of_bears_with_goofy_temperaments
 end
 
 def selects_bear_that_killed_Tim
-return "Select(*) From bears Where name = nil"
+return "Select (*) From bears Where name = 'nil';
 end
 
