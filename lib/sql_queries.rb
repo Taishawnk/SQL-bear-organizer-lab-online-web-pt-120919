f4c7cff5a1ee6980dcaca327a8a4128ea2031972
Taishawn King <taishawnking1@yahoo.com>
2020-04-16 16:41:15 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,13 +1,13 @@
 def selects_all_female_bears_return_name_and_age
-  "Write your SQL query here"
+return "SELECT name, age FROM bears WHERE gender = 'F' ";
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
-  "Write your SQL query here"
+  return "Select name From bears ORDER BY name ASC "
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  "Write your SQL query here"
+ return "Select name, age From bears Where "
 end
 
 def selects_oldest_bear_and_returns_name_and_age
@@ -29,3 +29,4 @@ end
 def selects_bear_that_killed_Tim
   "Write your SQL query here"
 end
+
